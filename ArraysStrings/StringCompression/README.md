1f611226a39fc64d6f90329486aad23750e5f79a
Steven Curtis <stevenpcurtis@hotmail.com>
2024-08-01 15:42:54 +0100

    update compression README

--- ArraysStrings/StringCompression/README.md
+++ ArraysStrings/StringCompression/README.md
@@ -39,7 +39,52 @@ class Solution {
 # A Simple Answer, Improved
 j will always be beyond i.
 ```swift
+class Solution {
+    func compression(_ string: String) -> String {
+        var solution = String()
+        
+        var i = 0
+        while i < string.count {
+            var j = i
+            let firstChar = string[string.index(string.startIndex, offsetBy: i)]
+            while j < string.count {
+                if firstChar == string[string.index(string.startIndex, offsetBy: j)] {
+                    j += 1
+                } else {
+                    break
+                }
+            }
+            solution.append("\(firstChar)\(j - i)")
+            i = j
+        }
+        return solution.count < string.count ? solution : string
+    }
+}
+```
 
+# Wouldn't it be better to cast to array?
+Yes, yes it would.
+
+```swift
+final class Solution {
+    func compression(_ string: String) -> String {
+        let str = Array(string)
+        var i = 0
+        var j = 0
+        var result = ""
+        while i < str.count {
+            var count = 0
+            let iLetter = str[i]
+            while j < str.count && iLetter == str[j] {
+                count += 1
+                j += 1
+            }
+            result.append("\(str[i])\(count)")
+            i = j
+        }
+        return result.count < string.count ? result : string
+    }
+}
 ```
 
 # Conclusion
--- ArraysStrings/StringCompression/StringCompressionPlayground.playground/Contents.swift
+++ ArraysStrings/StringCompression/StringCompressionPlayground.playground/Contents.swift
@@ -27,28 +27,47 @@ import XCTest
 //    }
 //}
 
-class Solution {
+//class Solution {
+//    func compression(_ string: String) -> String {
+//        var solution = String()
+//        
+//        var i = 0
+//        while i < string.count {
+//            var j = i
+//            let firstChar = string[string.index(string.startIndex, offsetBy: i)]
+//            while j < string.count {
+//                if firstChar == string[string.index(string.startIndex, offsetBy: j)] {
+//                    j += 1
+//                } else {
+//                    break
+//                }
+//            }
+//            solution.append("\(firstChar)\(j - i)")
+//            i = j
+//        }
+//        return solution.count < string.count ? solution : string
+//    }
+//}
+
+final class Solution {
     func compression(_ string: String) -> String {
-        var solution = String()
-        
+        let str = Array(string)
         var i = 0
-        while i < string.count {
-            var j = i
-            let firstChar = string[string.index(string.startIndex, offsetBy: i)]
-            while j < string.count {
-                if firstChar == string[string.index(string.startIndex, offsetBy: j)] {
-                    j += 1
-                } else {
-                    break
-                }
+        var j = 0
+        var result = ""
+        while i < str.count {
+            var count = 0
+            let iLetter = str[i]
+            while j < str.count && iLetter == str[j] {
+                count += 1
+                j += 1
             }
-            solution.append("\(firstChar)\(j - i)")
+            result.append("\(str[i])\(count)")
             i = j
         }
-        return solution.count < string.count ? solution : string
+        return result.count < string.count ? result : string
     }
 }
-
 
 final class SolutionTests: XCTestCase {
     private var solution: Solution!
@@ -58,7 +77,7 @@ final class SolutionTests: XCTestCase {
     
     func testSolution() {
         XCTAssertEqual(solution.compression("aabcccccaaa"), "a2b1c5a3")
-        XCTAssertEqual(solution.compression("ab"), "a1b1")
+        XCTAssertEqual(solution.compression("ab"), "ab")
     }
 }
 

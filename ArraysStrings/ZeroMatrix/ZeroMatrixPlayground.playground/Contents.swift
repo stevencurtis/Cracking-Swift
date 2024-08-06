afd29533cc51df11d2713f1217fcc0dea814b31e
Steven Curtis <stevenpcurtis@hotmail.com>
2024-08-06 17:39:54 +0100

    update arraystrings readme

--- ArraysStrings/ZeroMatrix/ZeroMatrixPlayground.playground/Contents.swift
+++ ArraysStrings/ZeroMatrix/ZeroMatrixPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
+import XCTest
 
-var greeting = "Hello, playground"
+// Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0.
+
+final class Solution {
+    func setZeroes(_ matrix: inout [[Int]]) {
+        
+    }
+}
+
+final class SolutionTest: XCTestCase {
+    private var solution: Solution!
+    override func setUp() {
+        self.solution = Solution()
+    }
+    func testSolution() {
+        var matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        solution.setZeroes(&matrix)
+        XCTAssertEqual(matrix, [[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
